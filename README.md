b14d9e99cc98b3aaef1fe92c181fdde9dd790546
Rodolfo Peixoto <rodolfog.peixoto@gmail.com>
2021-08-05 22:26:38 -0300

    Add project

---
+++ README.md
@@ -0,0 +1,2 @@
+# STUDIE GOLANG
+
---
+++ greetings/greetings.go
@@ -0,0 +1,17 @@
+package greetings
+
+import (
+    "fmt"
+    "errors"
+)
+
+// Hello returns a greeting for the named person.
+func Hello(name string) (string, error) {
+    //if no name was given, return an error with a message.
+    if name == ""{
+        return "", errors.New("empty name")
+    }
+    // Return a greeting that embeds the name in a message.
+    message := fmt.Sprintf("Hi, %v. Welcome!", name)
+    return message, nil
+}
